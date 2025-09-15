7c27b22303cb3d50e576217569738a74deba5917
Benjamin Aarsen <bj.aarsen@student.han.nl>
2025-09-15 16:19:25 +0200

    Error afhandeling gemaakt

--- low_level_driver/include/robot_arm_driver/RobotArmDriver.hpp
+++ low_level_driver/include/robot_arm_driver/RobotArmDriver.hpp
@@ -2,19 +2,43 @@
 
 #include "robot_arm_driver/SerialDriver.hpp"
 #include <string>
+#include <optional>
 
 class RobotArmDriver
 {
 public:
-    struct ServoCommand {
+    RobotArmDriver(const std::string &port = "/dev/ttyACM0", uint32_t baudrate = 9600);
+    ~RobotArmDriver();
+    struct ServoCommand
+    {
         uint8_t channel;
         uint16_t pulseWidth;
-        uint16_t speed;
-        uint16_t time;
+        std::optional<uint16_t> speed;
+        std::optional<uint16_t> time;
+    };
+
+    struct RobotArmDriverError
+    {
+        enum Code : int32_t
+        {
+            NONE = 0,
+            SERIAL_ERROR = -1,
+            INVALID_COMMAND = -2,
+            UNKNOWN_ERROR = -99
+        };
+        Code code;
+        std::string message;
     };
 
     typedef std::vector<ServoCommand> MultiServoCommand;
 
+    RobotArmDriverError sendSingleServoCommand(const ServoCommand &command);
+    RobotArmDriverError sendMultiServoCommand(const MultiServoCommand &commands);
+
 private:
+    std::string toSerialString(const ServoCommand &command);
+    std::string toSerialString(const MultiServoCommand &commands);
+    RobotArmDriverError validateCommand(const ServoCommand &command);
+    RobotArmDriverError validateCommands(const MultiServoCommand &commands);
     SerialDriver serial_driver_;
 };
