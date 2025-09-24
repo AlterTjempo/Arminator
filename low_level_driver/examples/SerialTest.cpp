668c471bf7615640c97102404722ad4be778224f
Benjamin Aarsen <bj.aarsen@student.han.nl>
2025-09-24 15:01:48 +0200

    extra argumenten

--- low_level_driver/examples/SerialTest.cpp
+++ low_level_driver/examples/SerialTest.cpp
@@ -4,30 +4,47 @@
 
 #include "RobotArmDriver.hpp"
 
-//Arguments: port, baudrate, angle, time_ms (optional)
-#define NUMBER_OF_ARGUMENTS 4
+// Arguments: port, baudrate, pulsewidth, time_ms (optional)
+#define MIN_NUMBER_OF_ARGUMENTS 5
 
-int main(int argc, char const *argv[]){
-	if(argc < NUMBER_OF_ARGUMENTS){
-		std::cerr << "Please provide a port, baudrate, angle (time optional)" << '\n';
+int main(int argc, char const *argv[])
+{
+	if (argc < MIN_NUMBER_OF_ARGUMENTS)
+	{
+		std::cerr << "Please provide a port, baudrate, channel, pulsewidth, time(optional), speed(optional)" << '\n';
 		return -1;
 	}
 
 	int status = 0;
 	std::string port = argv[1];
 	int baudrate = std::stoi(argv[2]);
-	int angle = std::stoi(argv[3]);
-	int time_ms = -1;
+	int channel = std::stoi(argv[3]);
+	int pulsewidth = std::stoi(argv[4]);
 
-	// //Make a servo controller object
-	// RobotArmDriver controller(port, baudrate);
+	RobotArmDriver driver(port, baudrate);
 
-	// if(argc == 5){
-	// 	time_ms = std::stoi(argv[4]);
-	// 	status = controller.setTimedAngle(angle, time_ms);
-	// }else{
-	// 	status = controller.setAngle(angle);
-	// }
-	// std::cout << "Exited with code: " << status << '\n';
+	RobotArmDriver::ServoCommand command;
+
+	if (argc == 6)
+	{
+		int time = std::stoi(argv[5]);
+		command.time = time;
+	}
+	if (argc == 7)
+	{
+		int speed = std::stoi(argv[6]);
+		command.speed = speed;
+	}
+
+	command.channel = channel;
+	command.pulseWidth = pulsewidth;
+
+	// std::cout << command.time.value() << std::endl;
+	auto error = driver.sendSingleServoCommand(command);
+	if (error.code != RobotArmDriver::RobotArmDriverError::NONE)
+	{
+		std::cerr << "Error sending command: " << error.message << '\n';
+		status = -1;
+	}
 	return status;
 }
