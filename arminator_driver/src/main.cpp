0555911805c0c2945b4ddf6e96f5e9f5f04fb8b4
Benjamin Aarsen <bj.aarsen@student.han.nl>
2025-09-29 16:07:46 +0200

    intelissense fix

--- arminator_driver/src/main.cpp
+++ arminator_driver/src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "RobotArmDriver.hpp"
+
 int main(int argc, char const *argv[])
 {
     // Suppress unused parameter warnings
     (void)argc;
     (void)argv;
-    
     RobotArmDriver driver("/dev/ttyUSB0", 115200);
 
     driver.sendSingleServoCommand({5, 2500, std::nullopt, std::nullopt});
