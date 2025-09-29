fe99182c40e4ddf96de3bb07153dd17cb9f1611c
Benjamin Aarsen <bj.aarsen@student.han.nl>
2025-09-29 14:38:20 +0200

    fixed stuff

---
+++ arminator_driver/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "RobotArmDriver.hpp"
+int main(int argc, char const *argv[])
+{
+    // Suppress unused parameter warnings
+    (void)argc;
+    (void)argv;
+    
+    RobotArmDriver driver("/dev/ttyUSB0", 115200);
+
+    driver.sendSingleServoCommand({5, 2500, std::nullopt, std::nullopt});
+    std::cout << "Hello, Arminator!" << std::endl;
+    return 0;
+}
