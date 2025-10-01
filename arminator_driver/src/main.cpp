2965733bb2fbe3872a35b260f1d70f8dbe6769a0
Benjamin Aarsen <bj.aarsen@student.han.nl>
2025-10-01 15:09:17 +0200

    park stand en remove srv

--- arminator_driver/src/main.cpp
+++ arminator_driver/src/main.cpp
@@ -2,7 +2,6 @@
 #include "std_srvs/srv/trigger.hpp"
 #include "arminator_driver/srv/move_servo.hpp"
 #include "arminator_driver/srv/set_position.hpp"
-#include "arminator_driver/srv/estop.hpp"
 
 
 #include <iostream>
@@ -39,6 +38,25 @@ void estop(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::
     response->message = "Emergency stop activated";
 }
 
+void park(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response){
+    (void)request; // Suppress unused parameter warning
+    RobotArmDriver::MultiServoCommand parkCommand = {
+        {1, 2200, 200, std::nullopt},
+        {2, 2000, 200, std::nullopt},
+        {3, 800, 200, std::nullopt},
+        {4, 1500, 200, std::nullopt},
+        {5, 1900, 200, std::nullopt},
+    };
+    RobotArmDriver::RobotArmDriverError error = driver.sendMultiServoCommand(parkCommand);
+    if(error.code == RobotArmDriver::RobotArmDriverError::Code::NONE){
+        response->success = true;
+        response->message = "Robot parked successfully";
+    } else {
+        response->success = false;
+        response->message = "Failed to park robot";
+    }
+}
+
 int main(int argc, char const *argv[]) {
     rclcpp::init(argc, argv);
     std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("arminator_driver_node");
@@ -52,6 +70,9 @@ int main(int argc, char const *argv[]) {
     
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr estop_service = 
         node->create_service<std_srvs::srv::Trigger>("estop", &estop);
+
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr park_service = 
+        node->create_service<std_srvs::srv::Trigger>("park", &park);
     
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Arminator driver services ready.");
     rclcpp::spin(node);
