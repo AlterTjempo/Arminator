98c2198884d903996f64c3d4be16da056de77485
Timo <t.berendsen1@student.han.nl>
2025-10-02 16:18:08 +0200

    Made a map instead of the copy-hell

--- arminator_driver/src/main.cpp
+++ arminator_driver/src/main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 #include "RobotArmDriver.hpp"
+#include "arminator_driver/positions.h"
+
 
 RobotArmDriver driver("/dev/ttyUSB0", 115200);
 
@@ -35,68 +37,31 @@ void estop(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::
     (void)request; // Suppress unused parameter warning
     driver.stopAllServos();
     response->success = true;
-    response->message = "Emergency stop activated";
+    response->message = "Emergency stop!";
 }
 
-void park(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response){
-    (void)request; // Suppress unused parameter warning
-    RobotArmDriver::MultiServoCommand parkCommand = {
-        {1, 2200, 200, std::nullopt},
-        {2, 2000, 200, std::nullopt},
-        {3, 800, 200, std::nullopt},
-        {4, 1450, 200, std::nullopt},
-        {5, 1900, 200, std::nullopt},
-    };
-    RobotArmDriver::RobotArmDriverError error = driver.sendMultiServoCommand(parkCommand);
-    if(error.code == RobotArmDriver::RobotArmDriverError::Code::NONE){
+void moveToPredefinedPosition(
+    const std::shared_ptr<std_srvs::srv::Trigger::Request> request [[maybe_unused]],
+    std::shared_ptr<std_srvs::srv::Trigger::Response> response, Position pos ){
+        
+    auto it = positions.find(pos);
+    if (it == positions.end()) {
+        response->success = false;
+        response->message = "Unknown position";
+        return;
+    }
+
+    RobotArmDriver::RobotArmDriverError error = driver.sendMultiServoCommand(it->second);
+
+    if (error.code == RobotArmDriver::RobotArmDriverError::Code::NONE) {
         response->success = true;
-        response->message = "Robot parked successfully";
+        response->message = "Success";
     } else {
         response->success = false;
-        response->message = "Failed to park robot";
+        response->message = "Failed";
     }
 }
 
-void straight_up(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response){
-    (void)request; // Suppress unused parameter warning
-    RobotArmDriver::MultiServoCommand straightUpCommand = {
-        {1, 1550, 200, std::nullopt},
-        {2, 650, 200, std::nullopt},
-        {3, 1450, 200, std::nullopt},
-        {4, 1450, 200, std::nullopt},
-        {5, 2500, 200, std::nullopt},
-    };
-
-    RobotArmDriver::RobotArmDriverError error = driver.sendMultiServoCommand(straightUpCommand);
-    if(error.code == RobotArmDriver::RobotArmDriverError::Code::NONE){
-        response->success = true;
-        response->message = "Robot moved to straight up position successfully";
-    } else {
-        response->success = false;
-        response->message = "Failed to move robot to straight up position";
-    }
-}
-
-
-void ready(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response){
-    (void)request; // Suppress unused parameter warning
-    RobotArmDriver::MultiServoCommand readyCommand = {
-        {1, 1850, 400, std::nullopt},
-        {2, 1650, 400, std::nullopt},
-        {3, 1300, 400, std::nullopt},
-        {4, 1500, 400, std::nullopt},
-        {5, 1000, 400, std::nullopt},
-    };
-
-    RobotArmDriver::RobotArmDriverError error = driver.sendMultiServoCommand(readyCommand);
-    if(error.code == RobotArmDriver::RobotArmDriverError::Code::NONE){
-        response->success = true;
-        response->message = "Robot moved to ready position successfully";
-    } else {
-        response->success = false;
-        response->message = "Failed to move robot to ready position";
-    }
-}
 
 
 int main(int argc, char const *argv[]) {
@@ -113,14 +78,25 @@ int main(int argc, char const *argv[]) {
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr estop_service = 
         node->create_service<std_srvs::srv::Trigger>("estop", &estop);
 
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr park_service = 
-        node->create_service<std_srvs::srv::Trigger>("park", &park);
+    // rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr park_service = 
+    //     node->create_service<std_srvs::srv::Trigger>("park", &park);
 
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr straight_up_service = 
-        node->create_service<std_srvs::srv::Trigger>("straight_up", &straight_up);
+    // rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr straight_up_service = 
+    //     node->create_service<std_srvs::srv::Trigger>("straight_up", &straight_up);
 
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr ready_service = 
-        node->create_service<std_srvs::srv::Trigger>("ready", &ready);
+    // rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr ready_service = 
+    //     node->create_service<std_srvs::srv::Trigger>("ready", &ready);
+
+    using namespace std::placeholders;
+    
+    auto park_service = node->create_service<std_srvs::srv::Trigger>("park",
+    std::bind(moveToPredefinedPosition, _1, _2, Position::Park));
+
+    auto straight_up_service = node->create_service<std_srvs::srv::Trigger>("straight_up",
+        std::bind(moveToPredefinedPosition, _1, _2, Position::StraightUp));
+
+    auto ready_service = node->create_service<std_srvs::srv::Trigger>("ready",
+        std::bind(moveToPredefinedPosition, _1, _2, Position::Ready));
     
     
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Arminator driver services ready.");
